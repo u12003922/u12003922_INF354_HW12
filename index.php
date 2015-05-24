529c72bc90aade011f7c43d5dacf2f98aa3df140
Edwin <EdwinRF@users.noreply.github.com>
2015-05-24 22:01:48 +0200

    Added details

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 	
 	echo "<p>Name &emsp;&emsp;&emsp; &verbar; Student Number </p>";
 	echo "<p>Ryno Pierce &nbsp;&nbsp; &verbar; 12003922</p>";
-	
+	echo "<p>Edwin Fullard &nbsp;&nbsp; &verbar; 12048675</p>";
 
 ?>
